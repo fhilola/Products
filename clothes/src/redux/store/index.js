1eb07b6a1b2111600b91256de8c5bf94052d7fd6
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-23 03:34:54 +0500

    redux-persist created

--- clothes/src/redux/store/index.js
+++ clothes/src/redux/store/index.js
@@ -1,6 +1,15 @@
 import {createStore } from 'redux'
+import rootReducer from '../reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = createStore()
+const persistConfig = {
+    key: 'reducer',
+    storage: storage,
+    whitelist: ['cart', 'like']
+}
 
-
+const presistReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(presistReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+persistStore()
 export default store
